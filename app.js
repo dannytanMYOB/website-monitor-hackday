a3801196755542a889c18619cdd9952851bf96d9
Anuradha Dunuhappawa <anuradha.dunuhappawa@myob.com>
2018-05-18 09:15:00 +1000

    Clean up app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var apiService = require('./services/apiService');
 var monitoringService = require('./services/monitoringService');
+var webpageController = require('./controllers/webpageController');
 
 var app = express();
 
@@ -17,79 +18,7 @@ var AU_URL = 'https://www.myob.com/au'
 var NZ_URL = 'https://www.myob.com/nz'
 
 // call the scraping service after the initial call
-getPageLinks();
-
-
-function getPageLinks() {
-  var sites = []
-  sites.push(apiService.checkUrl(AU_URL))
-  sites.push(apiService.checkUrl(NZ_URL))
-
-  Promise.all(sites)
-    .then(([auHTML, nzHTML]) => {
-      if (auHTML.status === '200') {
-        //return scrapingService.scrape({auHTML, nzHTML})
-        console.log('call scrapers');
-
-        // var eventDetails = {
-        //   errorId: 'website',
-        //   priorityLevel: 'P1',
-        //   country: 'au',
-        //   status: 'au',
-        //   hostname: 'hostname',
-        //   errorCategory: 'Website',
-        //   errorEndpoint: 'myob.com/au'
-        // };
-        //
-        // monitoringService.record({}, eventDetails)
-        //   .then((response) => console.log(response))
-        //   .catch((error) => console.error(error));
-      } else {
-        console.log('calling elastic search');
-        // 404 - elastic search
-        //call record method of montiorimg service
-        // error category => website
-        // host name
-        // priority level P1
-        // status failure
-        // country
-        // error endpoint pass host name
-        // status code 404
-        //error message
-        // error hostname
-
-
-        var errorDetails = {
-          errorId: 'website',
-          priorityLevel: 'P1',
-          country: 'au',
-          status: 'au',
-          hostname: 'hostname',
-          errorCategory: 'apiError',
-          errorEndpoint: 'myob.com/au'
-        };
-
-        var err = {
-          statusCode: '404',
-          errorMsg: 'Server unavailable',
-          hostname: 'dev'
-        };
-
-        monitoringService.record(err, errorDetails)
-          .then((response) => console.log(response))
-          .catch((error) => console.error(error));
-      }
-    }).then(() => {
-      var auHTML = {
-        links: ['au']
-      }
-      auHTML.links.forEach((link) => {
-        apiService.checkUrl(link);
-      })
-    })
-}
-
-
+webpageController.getWebpageStatus();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
