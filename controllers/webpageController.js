a4ca7aae4933fddf79f535013a79c51a358ca164
Natalie Riga <Natalie.Riga@myob.com>
2018-05-18 11:22:19 +1000

    moved scraperController from controller to service folder and commented out testing code

--- controllers/webpageController.js
+++ controllers/webpageController.js
@@ -1,6 +1,6 @@
 var apiService = require('../services/apiService');
 var monitoringService = require('../services/monitoringService');
-var monitoringService = require('../services/monitoringService');
+var scraperService = require('../services/scraperService');
 
 var AU_URL = 'https://www.myob.com/au';
 var NZ_URL = 'https://www.myob.com/nz';
@@ -15,7 +15,7 @@ function getWebpageStatus() {
       console.log('AU HTML RESPONSE ', auHTML.status);
       console.log('NZ HTML RESPONSE ', nzHTML.status);
       if (auHTML.status === 200){
-        //return scrapingService.scrape({auHTML, nzHTML})
+        // return scraperService.scrape(auHTML)
         console.log('call scrapers');
         // Form Document
         var eventDocument = {
